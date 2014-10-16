62e1cdced7e8045fd8eaacadb71057ef81c9c537
Mitar <mitar.git@tnode.com>
2014-10-16 02:36:33 -0700

    Migrated to Meteor 0.9.0.
    Conflicts:
    	package.js

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-  summary: "A reactive router built on page.js"
+  summary: "A reactive router built on page.js",
+  version: "0.6.1",
+  git: "https://github.com/tmeasday/meteor-router.git"
 });
 
 Npm.depends({
@@ -7,18 +9,20 @@ Npm.depends({
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('deps', 'client');
   api.use('startup', 'client');
   api.use('templating', 'client');
   api.use('handlebars', 'client');
-  api.use('page-js-ie-support', 'client');
+  api.use("tmeasday:page-js-ie-support@1.3.5", 'client');
   api.use('underscore', ['client', 'server']);
   
   // for backward compat before Meteor linker changes
   if (typeof api.export !== 'undefined') {
     api.use('webapp', 'server');
-    api.use('ui', 'client', {weak: true}); //For helpers
-    api.use('HTML5-History-API', 'client', {weak: true});
+    // Disabled to not cause errors on Meteor < 0.8
+    //api.use('ui', 'client', {weak: true}); //For helpers
+    api.use("tmeasday:html5-history-api@4.1.2", 'client', {weak: true});
   }
   
   api.add_files('lib/router_client.js', 'client');
@@ -29,7 +33,7 @@ Package.on_use(function (api, where) {
 
 
 Package.on_test(function (api) {
-  api.use('router', ['client', 'server']);
+  api.use("meteor-router-new", ['client', 'server']);
   api.use('test-helpers', ['client', 'server']);
   api.use('tinytest', ['client', 'server']);
   
